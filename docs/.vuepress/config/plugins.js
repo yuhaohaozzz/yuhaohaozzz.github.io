11ac8e0d40eae0a0392437e27e6e64899ede4619
喻胜银 <yushengyin@CN0014010803M.local>
2024-04-02 15:03:54 +0800

    test

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -71,10 +71,10 @@ module.exports = [
     "dynamic-title",
     {
       showIcon:
-        "https://wiki.eryajf.net/img/favicon.ico",
+        "https://raw.githubusercontent.com/yumuyan/blog/main/headblog.jpg",
       showText: "ヾ(≧▽≦*)o欢迎回来！",
       hideIcon:
-        "https://wiki.eryajf.net/img/favicon.ico",
+        "https://raw.githubusercontent.com/yumuyan/blog/main/headblog.jpg",
       hideText: "ಥ_ಥ不要走呀！",
       recoverTime: 2000, //  持续时间
     },
