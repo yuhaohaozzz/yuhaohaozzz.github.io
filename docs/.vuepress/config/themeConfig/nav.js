28017934eee6b6e192fe907b48b83793a0f86eff
喻胜银 <yushengyin@CN0014010803M.local>
2024-04-02 15:23:14 +0800

    add tools

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -43,13 +43,8 @@ module.exports = [
       {
         text: '我的工具',
         items: [
-          { text: '备忘录清单', link: 'https://ref.eryajf.net' },
-          { text: '网站状态', link: 'http://uptime.eryajf.net/dashboard' },
-          { text: 'json2go', link: 'http://public.eryajf.net/json2go' },
-          { text: '微信MD编辑', link: 'http://public.eryajf.net/mdnice' },
-          { text: '国内镜像', link: 'http://public.eryajf.net/mirror' },
-          { text: '出口IP查询', link: 'http://ip.eryajf.net' },
-          { text: '代码高亮工具', link: 'http://public.eryajf.net/highlight/' },
+          { text: '文心一言', link: 'https://yiyan.baidu.com' },
+          { text: 'POE.COM', link: 'https://poe.com' },
         ]
       }
     ],
