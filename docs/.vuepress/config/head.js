e76ea45d26da7271fbe3546ce4b2ad586ba1a319
喻胜银 <yushengyin@CN0014010803M.local>
2024-04-03 15:18:08 +0800

    test

--- docs/.vuepress/config/head.js
+++ docs/.vuepress/config/head.js
@@ -2,7 +2,7 @@
 module.exports = [
   // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
   ['link', { rel: 'icon', href: '/img/favicon.ico' }], //favicons，资源放在public文件夹
-  ['meta',{name: 'keywords',content: '二丫讲梵,Linux,运维,yum,运维艺术,eryajf,jenkins,nexus,golang,vue'}],
+  ['meta',{name: 'keywords',content: '运维,cicd,编程,生活'}],
   ['meta', { name: 'baidu-site-verification', content: 'code-LTKHwOecxI' }], // 百度统计的站长验证
   ['meta', { name: 'theme-color', content: '#11a8cd' }], // 移动浏览器主题颜色
   ['meta', {name: 'referrer', content: 'no-referrer-when-downgrade'}],
